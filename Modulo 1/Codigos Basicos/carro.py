6cabf8847dd789cdab254e49a73b131ccaa804e5
Eduardo151187 <marklet319@gmail.com>
2025-09-15 09:27:07 -0300

    Add files via upload

---
+++ Modulo 1/Codigos Basicos/carro.py
@@ -0,0 +1,9 @@
+Nome_carro = input("Digite o nome do carro")
+Valo_carro = float (input("digite o valor do carro :r$"))
+consumo_por_litro = input("digite o consumo por litro")
+
+print("---------------------------")
+print(f"| carro: {nome_carro}")
+print(f"| valor R${valor_carro:.2f}")
+print(f"| consumo: {consumo_por_litro}km/1")
+print("---------------------------")
